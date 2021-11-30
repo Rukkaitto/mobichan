5b4ac134ebd7a92a4168e1662b98a60823d1fac0
Rukkaitto <theprocraft@gmail.com>
2021-11-30 15:37:00 +0100

    Refactoring

--- lib/core/cubits/post_form_cubit/post_form_state.dart
+++ lib/core/cubits/post_form_cubit/post_form_state.dart
@@ -5,26 +5,20 @@ class PostFormState extends Equatable {
   final TextEditingController nameController = TextEditingController();
   final TextEditingController subjectController = TextEditingController();
 
-  static const double contractedHeight = 190;
-  static const double expandedHeight = 320;
   static const double imageHeight = 50;
 
   final String comment;
   final bool isVisible;
-  final double height;
+  final bool isExpanded;
   final XFile? file;
 
   PostFormState({
     this.comment = '',
     this.isVisible = false,
-    this.height = contractedHeight,
+    this.isExpanded = false,
     this.file,
   });
 
   @override
-  List<Object?> get props => [comment, isVisible, height, file];
-
-  bool get isExpanded => height == expandedHeight;
-
-  double get heightWithImage => height + (file != null ? imageHeight : 0);
+  List<Object?> get props => [comment, isVisible, isExpanded, file];
 }
