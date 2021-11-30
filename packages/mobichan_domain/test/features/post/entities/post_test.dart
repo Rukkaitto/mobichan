205edb714fcb63ed345d9c53b350091de2777345
Rukkaitto <theprocraft@gmail.com>
2021-11-30 16:35:52 +0100

    Added getReplies unit tests

--- packages/mobichan_domain/test/features/post/entities/post_test.dart
+++ packages/mobichan_domain/test/features/post/entities/post_test.dart
@@ -3,44 +3,56 @@ import 'package:mobichan_domain/mobichan_domain.dart';
 
 void main() {
   group('Post', () {
+    const firstPost = Post(
+      no: 123123,
+      com: """
+        this is a comment replying to no one
+      """,
+    );
+    const secondPost = Post(
+      no: 234234,
+      com: """
+        <a href="#p123123" class="quotelink">&gt;&gt;123123
+        this is a comment replying to the first post
+      """,
+    );
+    const thirdPost = Post(
+      no: 345345,
+      com: """
+        <a href="#p123123" class="quotelink">&gt;&gt;123123
+        this is another comment replying to the first post
+      """,
+    );
+
+    List<Post> posts = const [
+      firstPost,
+      secondPost,
+      thirdPost,
+    ];
+
+    group('getReplies', () {
+      test('should return empty list if the post has no replies', () {
+        final replies = secondPost.getReplies(posts);
+        expect(replies, []);
+      });
+
+      test('should return a list of replies to a post', () {
+        final replies = firstPost.getReplies(posts);
+        expect(replies.isEmpty, false);
+        expect(replies, [secondPost, thirdPost]);
+      });
+    });
+
     group('replyingTo', () {
-      const firstPost = Post(no: 123123);
-      const secondPost = Post(no: 234234);
-      const thirdPost = Post(no: 345345);
-
-      List<Post> posts = const [
-        firstPost,
-        secondPost,
-        thirdPost,
-      ];
-
       test('should return empty list if the post is replying to no one', () {
-        Post post = const Post(
-          com: """
-            this is a comment with no replies
-          """,
-        );
-
-        final replyingTo = post.getReplies(posts);
+        final replyingTo = firstPost.replyingTo(posts);
         expect(replyingTo, []);
       });
 
       test('should return a list of posts that the post is replying to', () {
-        Post post = const Post(
-          com: """
-            <a href="#p123123" class="quotelink">&gt;&gt;84520835
-            this is a reply
-            <a href="#p345345" class="quotelink">&gt;&gt;84520835
-            this is another reply
-            <a href="#p999999" class="quotelink">&gt;&gt;84520835
-            this is a reply to a post that doesn't exist
-          """,
-        );
-
-        final replyingTo = post.replyingTo(posts);
+        final replyingTo = secondPost.replyingTo(posts);
         expect(replyingTo.isEmpty, false);
         expect(replyingTo.contains(firstPost), true);
-        expect(replyingTo.contains(thirdPost), true);
       });
     });
   });
