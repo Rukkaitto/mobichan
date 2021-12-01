402d59f310a15137a9e8b1a8f2f50439190d30a2
Rukkaitto <theprocraft@gmail.com>
2021-12-01 11:32:47 +0100

    Added userName tests

--- packages/mobichan_domain/test/features/post/entities/post_test.dart
+++ packages/mobichan_domain/test/features/post/entities/post_test.dart
@@ -4,23 +4,24 @@ import 'package:mobichan_domain/mobichan_domain.dart';
 void main() {
   group('Post', () {
     const tBoard = Board(board: 'g', title: 'Technology', wsBoard: 1);
-    const tFirstPost = Post(
+    const tOP = Post(
       no: 123123,
       sub: 'This is a subject',
+      name: 'Name',
       com: """
         This is an OP
       """,
       tim: 7839789,
       ext: '.png',
     );
-    const tSecondPost = Post(
+    const tNoFileReply = Post(
       no: 234234,
       com: """
         <a href="#p123123" class="quotelink">&gt;&gt;123123
         this is a comment replying to the first post
       """,
     );
-    const tThirdPost = Post(
+    const tWebmReply = Post(
       no: 345345,
       com: """
         <a href="#p123123" class="quotelink">&gt;&gt;123123
@@ -29,52 +30,52 @@ void main() {
       tim: 4792974,
       ext: '.webm',
     );
-    const tFourthPost = Post(
+    const tImagePost = Post(
       no: 794794,
       tim: 9483945,
       ext: '.png',
     );
 
     List<Post> posts = const [
-      tFirstPost,
-      tSecondPost,
-      tThirdPost,
+      tOP,
+      tNoFileReply,
+      tWebmReply,
     ];
 
     group('getReplies', () {
       test('should return empty list if the post has no replies', () {
-        final replies = tSecondPost.getReplies(posts);
+        final replies = tNoFileReply.getReplies(posts);
         expect(replies, []);
       });
 
       test('should return a list of replies to a post', () {
-        final replies = tFirstPost.getReplies(posts);
+        final replies = tOP.getReplies(posts);
         expect(replies.isEmpty, false);
-        expect(replies, [tSecondPost, tThirdPost]);
+        expect(replies, [tNoFileReply, tWebmReply]);
       });
     });
 
     group('replyingTo', () {
       test('should return empty list if the post is replying to no one', () {
-        final replyingTo = tFirstPost.replyingTo(posts);
+        final replyingTo = tOP.replyingTo(posts);
         expect(replyingTo, []);
       });
 
       test('should return a list of posts that the post is replying to', () {
-        final replyingTo = tSecondPost.replyingTo(posts);
+        final replyingTo = tNoFileReply.replyingTo(posts);
         expect(replyingTo.isEmpty, false);
-        expect(replyingTo.contains(tFirstPost), true);
+        expect(replyingTo.contains(tOP), true);
       });
     });
 
     group('isRootPost', () {
       test('should return true if the post is a root post', () {
-        final isRootPost = tFirstPost.isRootPost;
+        final isRootPost = tOP.isRootPost;
         expect(isRootPost, true);
       });
 
       test('should return false if the post is not a root post', () {
-        final isRootPost = tSecondPost.isRootPost;
+        final isRootPost = tNoFileReply.isRootPost;
         expect(isRootPost, false);
       });
     });
@@ -86,73 +87,85 @@ void main() {
       });
 
       test('should return a post if given a list of posts and a quotelink', () {
-        final post = posts.getQuotedPost("#p${tFirstPost.no}");
-        expect(post, tFirstPost);
+        final post = posts.getQuotedPost("#p${tOP.no}");
+        expect(post, tOP);
       });
     });
 
     group('getImageUrl', () {
       test('should return null if the post has no image', () {
-        final imageUrl = tSecondPost.getImageUrl(tBoard);
+        final imageUrl = tNoFileReply.getImageUrl(tBoard);
         expect(imageUrl, null);
       });
       test('should return the url of the image if the post has an image', () {
-        final imageUrl = tFirstPost.getImageUrl(tBoard);
+        final imageUrl = tOP.getImageUrl(tBoard);
         expect(
           imageUrl,
-          "https://i.4cdn.org/${tBoard.board}/${tFirstPost.tim}${tFirstPost.ext}",
+          "https://i.4cdn.org/${tBoard.board}/${tOP.tim}${tOP.ext}",
         );
       });
     });
 
     group('getThumbnailUrl', () {
       test('should return null if the post has no image', () {
-        final imageUrl = tSecondPost.getThumbnailUrl(tBoard);
+        final imageUrl = tNoFileReply.getThumbnailUrl(tBoard);
         expect(imageUrl, null);
       });
       test('should return the url of the thumbnail if the post has an image',
           () {
-        final imageUrl = tFirstPost.getThumbnailUrl(tBoard);
+        final imageUrl = tOP.getThumbnailUrl(tBoard);
         expect(
           imageUrl,
-          "https://i.4cdn.org/${tBoard.board}/${tFirstPost.tim}s.jpg",
+          "https://i.4cdn.org/${tBoard.board}/${tOP.tim}s.jpg",
         );
       });
     });
 
     group('isWebm', () {
       test('should return false if the post has no file', () {
-        final isWebm = tSecondPost.isWebm;
+        final isWebm = tNoFileReply.isWebm;
         expect(isWebm, false);
       });
 
       test('should return false if the post has an image', () {
-        final isWebm = tFirstPost.isWebm;
+        final isWebm = tOP.isWebm;
         expect(isWebm, false);
       });
 
       test('should return true if the file is a webm', () {
-        final isWebm = tThirdPost.isWebm;
+        final isWebm = tWebmReply.isWebm;
         expect(isWebm, true);
       });
     });
 
     group('displayTitle', () {
       test('should return the subject of the post if there is one', () {
-        final displayTitle = tFirstPost.displayTitle;
-        expect(displayTitle, tFirstPost.sub);
+        final displayTitle = tOP.displayTitle;
+        expect(displayTitle, tOP.sub);
       });
 
       test('should return the comment of the post if there is no subject', () {
-        final displayTitle = tSecondPost.displayTitle;
-        expect(displayTitle, tSecondPost.com);
+        final displayTitle = tNoFileReply.displayTitle;
+        expect(displayTitle, tNoFileReply.com);
       });
 
       test(
           'should return an empty string if there is no subject and no comment',
           () {
-        final displayTitle = tFourthPost.displayTitle;
+        final displayTitle = tImagePost.displayTitle;
         expect(displayTitle, '');
+      });
+    });
+
+    group('userName', () {
+      test('should return "Anonymous" if the poster has no name', () {
+        final userName = tImagePost.userName;
+        expect(userName, "Anonymous");
+      });
+
+      test('should return the name of the poster if they have one', () {
+        final userName = tOP.userName;
+        expect(userName, tOP.name);
       });
     });
   });
