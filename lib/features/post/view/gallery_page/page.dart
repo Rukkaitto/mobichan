4e65d845022c904024abedb7aa8abf556fa1db5f
Rukkaitto <theprocraft@gmail.com>
2021-12-20 16:27:40 +0100

    Added high resolution thumbnails to gallery page

--- lib/features/post/view/gallery_page/page.dart
+++ lib/features/post/view/gallery_page/page.dart
@@ -1,13 +1,8 @@
-import 'dart:io';
-
-import 'package:cached_network_image/cached_network_image.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:mobichan/features/post/post.dart';
 import 'package:mobichan_domain/mobichan_domain.dart';
 import 'package:mobichan/localization.dart';
 import 'package:easy_localization/easy_localization.dart';
-
-import 'gallery_page.dart';
 
 class GalleryPageArguments {
   final Board board;
@@ -49,17 +44,12 @@ class GalleryPage extends StatelessWidget {
               index: index,
             ),
             child: Hero(
-                tag: "image$index",
-                child: CachedNetworkImage(
-                  fit: BoxFit.cover,
-                  imageUrl: args.imagePosts[index].getThumbnailUrl(args.board)!,
-                  placeholder: (context, url) => Center(
-                    child: Platform.isAndroid
-                        ? const CircularProgressIndicator()
-                        : const CupertinoActivityIndicator(),
-                  ),
-                  errorWidget: (context, url, error) => const Icon(Icons.error),
-                )),
+              tag: "image$index",
+              child: ImageWidget(
+                board: args.board,
+                post: args.imagePosts[index],
+              ),
+            ),
           );
         },
       ),
